0d8c0f75d64bfe622c65329de6fb34e7ea6cf1e9
Matteo <outlaw.plz@gmail.com>
2024-02-16 00:39:03 +0100

    Improves treesitter and lsp keymap

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -13,12 +13,24 @@ return {
 
 		local lsp = require("lspconfig")
 
+		-- LSP
+
 		lsp.lua_ls.setup({})
 		lsp.intelephense.setup({})
 		lsp.volar.setup({})
+		lsp.tailwindcss.setup({})
 
-		vim.keymap.set("n", "<leader>lD", vim.lsp.buf.hover, {})
-		vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, {})
-		vim.keymap.set({ "n" }, "<leader>la", vim.lsp.buf.code_action, {})
+		-- Keymaps
+
+		vim.keymap.set("n", "gd", ":Telescope lsp_definitions<CR>", { desc = "Go to definition" })
+		vim.keymap.set("n", "ga", "<cmd>lua vim.lsp.buf.code_action()<CR>", { desc = "Go to code actions" })
+		vim.keymap.set("n", "gi", ":Telescope lsp_implementations<CR>", { desc = "Go to implementations" })
+		vim.keymap.set("n", "gr", ":Telescope lsp_references<CR>", { desc = "Go to references" })
+		-- vim.keymap.set("n", "<Leader>lr", ":LspRestart<CR>", { silent = true })
+		-- vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+		-- vim.keymap.set("n", "<Leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+		-- vim.keymap.set("n", "<Leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>")
+		-- vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
+		-- vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>")
 	end,
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,11 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
 	config = function()
-		local config = require("nvim-treesitter.configs")
-
-		config.setup({
+		require("nvim-treesitter.configs").setup({
 			ensure_installed = {
 				"bash",
 				"css",
@@ -24,6 +25,20 @@ return {
 			},
 			highlight = { enable = true },
 			indent = { enable = true },
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = { query = "@function.outer", desc = "around function" },
+						["if"] = { query = "@function.inner", desc = "inner function" },
+						["ac"] = { query = "@class.outer", desc = "around class" },
+						["ic"] = { query = "@class.inner", desc = "inner class" },
+						["aa"] = { query = "@parameter.outer", desc = "around attribute" },
+						["ia"] = { query = "@parameter.inner", desc = "inner attribute" },
+					},
+				},
+			},
 		})
 	end,
 }
