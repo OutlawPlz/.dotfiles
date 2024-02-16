8b83034fd4725eaf517e1233949d65463f79c377
Matteo <outlaw.plz@gmail.com>
2024-02-16 01:55:20 +0100

    wip

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -26,9 +26,10 @@ return {
 		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
 		vim.keymap.set("n", "gi", ":Telescope lsp_implementations<CR>", { desc = "Go to implementations" })
 		vim.keymap.set("n", "gr", ":Telescope lsp_references<CR>", { desc = "Go to references" })
-		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename occurrence" })
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename occurrence via LSP" })
 		vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { desc = "Lint and format code" })
-		vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, { desc = "Show documentation, [h]elp" })
+		vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, { desc = "Show documentation from LSP" })
+		vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagnostic"})
 
 		vim.api.nvim_create_autocmd("CursorHold", {
 			pattern = "*.*",
@@ -38,9 +39,6 @@ return {
 		})
 
 		-- vim.keymap.set("n", "<Leader>lr", ":LspRestart<CR>", { silent = true })
-		-- vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
-		-- vim.keymap.set("n", "<Leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
-		-- vim.keymap.set("n", "<Leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>")
 		-- vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
 		-- vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>")
 	end,
