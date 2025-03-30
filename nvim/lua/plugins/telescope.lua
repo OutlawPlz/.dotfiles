154c4d86e21532fb007c456afc11053647c8199c
Matteo <outlaw.plz@gmail.com>
2025-03-30 19:51:27 +0200

    Adds telescope-file-browser plugin

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,13 +1,16 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.5",
+	tag = "0.1.8",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
 	keys = {
-		{ "<leader><space>", ":Telescope find_files<CR>",                 { desc = "Find files" } },
-		{ "<leader><tab>",   ":Telescope buffers sort_lastused=true<CR>", { desc = "Find buffers" } },
-		{ "<leader>fg",      ":Telescope live_grep<CR>",                  { desc = "Grep text" } },
+		{ "<leader><space>", ":Telescope find_files<CR>",                                 { desc = "Find files" } },
+		{ "<leader><tab>",   ":Telescope buffers sort_lastused=true<CR>",                 { desc = "Find buffers" } },
+		{ "<leader>fa",      ":Telescope find_files no_ignore=true<CR>",                  { desc = "Find all files" } },
+		{ "<leader>fg",      ":Telescope live_grep<CR>",                                  { desc = "Grep text" } },
+		{ "<leader>fb",      ":Telescope file_browser path=%:p:h select_buffer=true<CR>", { desc = "Browse files" } },
+		{ "<leader>fs",      ":Telescope lsp_document_symbols<CR>",                       { desc = "Find symbols" } },
 	},
 	config = function()
 		local actions = require("telescope.actions")
@@ -26,6 +29,7 @@ return {
 					},
 				},
 				file_ignore_patterns = { ".git/" },
+				-- path_display = "truncate",
 			},
 			pickers = {
 				find_files = {
@@ -34,6 +38,15 @@ return {
 				buffers = {
 					previewer = false,
 					theme = "dropdown",
+				},
+			},
+			extensions = {
+				file_browser = {
+					previewer = false,
+					grouped = true,
+					layout_config = {
+						width = 80,
+					},
 				},
 			},
 		})
